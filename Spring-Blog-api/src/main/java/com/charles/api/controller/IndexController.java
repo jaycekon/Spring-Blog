da1808006bcbefcc1d6c5b33b00d95df3031e95d
jaycekon@163.com <jaycekong824>
2018-01-14 13:51:01 +0800

    fix contributions

--- Spring-Blog-api/src/main/java/com/charles/api/controller/IndexController.java
+++ Spring-Blog-api/src/main/java/com/charles/api/controller/IndexController.java
@@ -2,7 +2,9 @@ package com.charles.api.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * Created by Jaycekon on 2018/1/14.
@@ -11,10 +13,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class IndexController {
 
 
-
     @RequestMapping(value = "hello")
-    public String index(Model model){
+    public String index(Model model) {
         model.addAttribute("name", "Dear");
         return "hello";
+    }
+
+
+    @RequestMapping(value = "index")
+    @ResponseBody
+    public String hello() {
+        return "hello world!";
     }
 }
